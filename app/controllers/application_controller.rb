0acfd56a86da3f2a9178e243c16517e1f2d5771a
Diego Echeverri <diego@envoy.com>
2016-12-09 18:30:28 -0500

    Adding simple http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   check_authorization unless: :devise_controller?
-
+  http_basic_authenticate_with name: ENV["ADMIN"], password: ENV["ADMIN_PASSWORD"], except: :index
 private
   def after_sign_in_path_for(resource)
     apps_path
